ba23fa352ad829a710d7d0af5b7b8721a80344bb
FullSol <ceraines@student.fullsail.edu>
2023-11-05 14:42:19 -0500

    created streams file

---
+++ src/components/streams/streams.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Paper } from "@mui/material";
+
+const Streams = () => {
+  return (
+    <Container>
+      <Paper></Paper>
+    </Container>
+  );
+};
+
+export default Streams;
