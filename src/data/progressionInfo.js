3a019059748709aa4c71b6c19c0f9ae19aa1aaa2
FullSol <ceraines@student.fullsail.edu>
2023-11-05 21:42:31 -0500

    created progression info data file

---
+++ src/data/progressionInfo.js
@@ -0,0 +1,23 @@
+export const ProgressionInfo = {
+  current: [
+    { id: 1, name: "Gnarlroot", title: "Ancient of War", imgLocation: "" },
+    { id: 2, name: "Igira the Cruel", title: "Zaqali Elder", imgLocation: "" },
+    { id: 3, name: "Volcross", title: null, imgLocation: "" },
+    { id: 4, name: "Council of Dreams", title: "", imgLocation: "" },
+    {
+      id: 5,
+      name: "Larodar, Keeper of the Flames",
+      title: "",
+      imgLocation: "",
+    },
+    { id: 6, name: "Nymue", title: "Weaver of the Cycle", imgLocation: "" },
+    { id: 7, name: "Smolderon", title: "The Firelord", imgLocation: "" },
+    {
+      id: 8,
+      name: "Tindral Sageswift",
+      title: "Seer of the Flame",
+      imgLocation: "",
+    },
+    { id: 9, name: "Fyrakk", title: "The Blazing", imgLocation: "" },
+  ],
+};
