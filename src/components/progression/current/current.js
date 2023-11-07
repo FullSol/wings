376e21e8015ad635fbbf669aa5d7de46afb054d6
FullSol <ceraines@student.fullsail.edu>
2023-11-07 15:32:05 -0500

    Updates to the progression section
    - Added previous raid tiers for expansion
    - restyled the progression boss cards for current raid tier

--- src/components/progression/current/current.js
+++ src/components/progression/current/current.js
@@ -5,22 +5,41 @@ import BossProgressionCard from "./bossProgressionCard";
 const CurrentProgression = () => {
   const [state, setState] = useState({
     current: [],
+    difficulty: "normal",
   });
 
-  const { current } = state;
+  const { current, difficulty } = state;
 
   useEffect(() => {
+    const getDifficulty = () => {
+      let difficulty = "normal";
+      current.bosses?.forEach((boss) => {
+        if (boss.difficulties.mythic.status === "killed") difficulty = "mythic";
+        else if (boss.difficulties.heroic.status === "killed")
+          difficulty = "heroic";
+      });
+      return difficulty;
+    };
+
     import("../../../data/progressionInfo")
       .then(({ ProgressionInfo: data }) => {
         setState((prevState) => ({
           ...prevState,
-          current: data.current,
+          current: data[data.length - 1],
         }));
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+
+    const difficulty = getDifficulty();
+
+    setState((prevState) => ({
+      ...prevState,
+      difficulty: difficulty,
+    }));
+  }, [current.bosses]);
+
   return (
     <Grid
       container
@@ -34,7 +53,7 @@ const CurrentProgression = () => {
           <BossProgressionCard
             name={boss.name}
             imgLocation={boss.imgLocation}
-            progress={boss.lowest}
+            progress={boss.difficulties[difficulty].lowest}
           />
         );
       })}
