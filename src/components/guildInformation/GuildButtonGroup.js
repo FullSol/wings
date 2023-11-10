6c89312b3fba1268f7c92c435b4055d14e8727a5
FullSol <ceraines@student.fullsail.edu>
2023-11-10 08:06:16 -0500

    updated Button Group layout
    - refactored code to remove duplicated code

--- src/components/guildInformation/GuildButtonGroup.js
+++ src/components/guildInformation/GuildButtonGroup.js
@@ -1,42 +1,21 @@
 import React from "react";
-import { Tooltip, ButtonGroup } from "@mui/material";
+import { ButtonGroup } from "@mui/material";
 import GuildButton from "./GuildButton";
 
-const GuildButtonGroup = ({ buttons, tooltip, color }) => {
-  const renderGroup = () => {
-    if (tooltip) {
-      return (
-        <Tooltip title={tooltip}>
-          <ButtonGroup color={color}>
-            {buttons.map((button) => (
-              <GuildButton
-                key={button.id}
-                content={button.label}
-                link={button.link}
-                tooltip={button.tooltip}
-              />
-            ))}
-          </ButtonGroup>
-        </Tooltip>
-      );
-    }
-
-    // If no tooltip
-    return (
-      <ButtonGroup color={color}>
-        {buttons.map((button) => (
-          <GuildButton
-            key={button.id}
-            content={button.label}
-            link={button.link}
-            tooltip={button.tooltip}
-          />
-        ))}
-      </ButtonGroup>
-    );
-  };
-
-  return renderGroup();
+const GuildButtonGroup = ({ buttons, color }) => {
+  return (
+    <ButtonGroup color={color}>
+      {/* TODO: theme.palette.alliance */}
+      {buttons.map((button) => (
+        <GuildButton
+          key={button.id}
+          content={button.label}
+          link={button.link}
+          tooltip={button.tooltip}
+        />
+      ))}
+    </ButtonGroup>
+  );
 };
 
 export default GuildButtonGroup;
