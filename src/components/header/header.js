0bb93df346e8484ceeaf12a54bccc37342238458
FullSol <ceraines@student.fullsail.edu>
2023-11-11 10:08:15 -0500

    Update header.js
    - removed unused import

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { AppBar, Toolbar, Container } from "@mui/material";
-import { Navigation } from "./";
 import { Title } from "../headings";
 
 const Header = ({ title, links }) => {
